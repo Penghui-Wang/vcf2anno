bb9b6df2c87cb720a00ad83b97b3ef62afa59b5d
YangRui <yangrui@gene.ac>
2017-11-30 16:38:05 +0800

    init

---
+++ vcf2anno/annoVCF.py
@@ -0,0 +1,15 @@
+#python
+
+class Anno:
+    
+    def __init__(self, species):
+        self.species = species
+
+    def gene_based(self):
+        pass
+
+    def region_based(self):
+        pass
+
+    def filter_based(self):
+        pass
