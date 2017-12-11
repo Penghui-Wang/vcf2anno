4243ecd43df997e4d0b52304138bdb26a01ac6f1
wangpenghui <wangpenghui@gene.ac>
2017-12-11 08:55:19 +0800

    add annoVCF

--- tests/test_annoVCF.py
+++ tests/test_annoVCF.py
@@ -1,1 +1,9 @@
 #python
+import sys
+sys.path.appen("../")
+from vcf2anno.config import human_db,table_annovar,anno_db
+from vcf2anno.annoVCF.annoVCF import annoVCF
+
+prefix = ""
+input = ""
+
--- vcf2anno/annoVCF/annoVCF.py
+++ vcf2anno/annoVCF/annoVCF.py
@@ -1,4 +1,5 @@
-import os,sys
+# cofing:utf-8
+'''import os,sys
 from config import annovar
 class Anno:
     
@@ -16,3 +17,28 @@ class Anno:
 
     def filter_based(self):
         pass
+'''
+import sys
+from config import table_annovar,anno_db
+import glob
+import os
+
+class AnnoVCF:
+		
+	def __init__(self,avinput,db,species,prefix)
+		self.avinput = avinput
+		self.species = species
+		self.prefix = prefix
+		self.db = db
+		self.db_path = os.path.join(anno_db,self.species)
+
+	def gene_based(self,select_db = db):
+		pass
+	
+	def region_based(self,select_db = db):
+		pass
+
+	def filted_based(self,select_db = db):
+		pass
+
+		
