9d7786669a75fd50cd101a31a0f71a49d4ee8f23
wangpenghui <wangpenghui@gene.ac>
2017-12-12 19:15:02 +0800

    unremoved anno_all

--- vcf2anno/annoVCF/annoVCF.py
+++ vcf2anno/annoVCF/annoVCF.py
@@ -1,84 +1,100 @@
 # cofing:utf-8
-'''import os,sys
-from config import annovar
-class Anno:
-    
-    def __init__(self,avinput,db,species,prefix):
-        self.species = species
-		self.avinput = avinput
-
-    def gene_based(self):
-		annovar = 
-		cmd = "%s -out %s -build hg19 %s %s" % (annovar,inputfl,outfl,db)#table_annovar.pl
-		os.sys(cmd)
-
-    def region_based(self):
-        pass
-
-    def filter_based(self):
-        pass
-'''
 
 import sys
-from config import table_annovar,anno_db
+from config import table_annovar
 import os
 
 class AnnoVCF:
 		
-	def __init__(self,avinput,species,prefix,db=None):
+	def __init__(self,avinput,species,anno_db,prefix):
 		self.avinput = avinput
 		self.species = species
 		self.prefix = prefix
-		self.db = db
 		self.db_path = os.path.join(anno_db,self.species)
-
-	def gene_based(self):
-		genedb = os.path.join(self.db_path,"gene_based")
-		cmd = "%s %s --remove -buildver %s %s -protocol " % (table_annovar, self.avinput,self.species, genedb)
-		prodb_list = []
-		for dbfile in os.listdir(genedb):
+		self.genedb = os.path.join(self.db_path,"gene_based")
+		self.regiondb = os.path.join(self.db_path,"region_based")
+		self.filterdb = os.path.join(self.db_path,"filter_based")
+		self.genedb_list = []
+		self.regiondb_list = []
+		self.filterdb_list = []
+		for dbfile in os.listdir(self.genedb):
 			if os.path.splitext(dbfile)[1] == ".txt":
 				prodb = dbfile.split(self.species+'_')[-1].split(".")[0]
-				prodb_list.append(prodb)
-		cmd += ','.join(prodb_list)
-		cmd += ' -operation '+','.join(['g']*len(prodb_list))
+				self.genedb_list.append(prodb)
+		for dbfile in os.listdir(self.regiondb):
+			if os.path.splitext(dbfile)[1] == ".txt":
+				prodb = dbfile.split(self.species+'_')[-1].split(".")[0]
+				self.regiondb_list.append(prodb)
+		for dbfile in os.listdir(self.filterdb):
+			if os.path.splitext(dbfile)[1] == ".txt":
+				prodb = dbfile.split(self.species+'_')[-1].split(".")[0]
+				self.filterdb_list.append(prodb)
+
+
+	def gene_based(self):
+		cmd = "%s %s --remove -buildver %s %s -protocol " % (table_annovar, self.avinput,self.species, self.genedb)
+		cmd += ','.join(self.genedb_list)
+		cmd += ' -operation '+','.join(['g']*len(self.genedb_list))
 		cmd += ' -nastring . --outfile %s' % (self.prefix + '.Genebased')
 		print cmd
 		os.system(cmd)
-		outFile = self.prefix + '.GeneBased.' + self.species + '_multianno.txt'
+		outFile = self.prefix + '.Genebased.' + self.species + '_multianno.txt'
 		return outFile
 	
 	def region_based(self):
-		regiondb = os.path.join(self.db_path,"region_based")
-		cmd = "%s %s --remove -buildver %s %s -protocol " % (table_annovar, self.avinput,self.species, regiondb)
-		prodb_list = []
-		for dbfile in os.listdir(regiondb):
-			if os.path.splitext(dbfile)[1] == ".txt":
-				prodb = dbfile.split(self.species+'_')[-1].split(".")[0]
-				prodb_list.append(prodb)
-		cmd += ','.join(prodb_list)
-		cmd += ' -operation '+','.join(['r']*len(prodb_list))
+		cmd = "%s %s --remove -buildver %s %s -protocol " % (table_annovar, self.avinput,self.species, self.regiondb)
+		cmd += ','.join(self.regiondb_list)
+		cmd += ' -operation '+','.join(['r']*len(self.regiondb_list))
 		cmd += ' -nastring . --outfile %s' % (self.prefix + '.Regionbased')
 		print cmd
 		os.system(cmd)
-		outFile = self.prefix + '.RegionBased.' + self.species + '_multianno.txt'
+		outFile = self.prefix + '.Regionbased.' + self.species + '_multianno.txt'
 		return outFile
 
 
-	def filted_based(self):
-		filterdb = os.path.join(self.db_path,"filter_based")
-		cmd = "%s %s --remove -buildver %s %s -protocol " % (table_annovar, self.avinput,self.species,filterdb)
-		prodb_list = []
-		for dbfile in os.listdir(filterdb):
-			if os.path.splitext(dbfile)[1] == ".txt":
-				prodb = dbfile.split(self.species+'_')[-1].split(".")[0]
-				prodb_list.append(prodb)
-		cmd += ','.join(prodb_list)
-		cmd += ' -operation '+','.join(['f']*len(prodb_list))
+	def filter_based(self):
+		cmd = "%s %s --remove -buildver %s %s -protocol " % (table_annovar, self.avinput,self.species,self.filterdb)
+		cmd += ','.join(self.filterdb_list)
+		cmd += ' -operation '+','.join(['f']*len(self.filterdb_list))
 		cmd += ' -nastring . --outfile %s' % (self.prefix + '.Filterbased')
 		print cmd
 		os.system(cmd)
-		outFile = self.prefix + '.FilterBased.' + self.species + '_multianno.txt'
+		outFile = self.prefix + '.Filterbased.' + self.species + '_multianno.txt'
 		return outFile
+
+
+	
+'''	def anno_alldb(self):
+		cmd = "%s %s --remove -buildver %s %s -protocol " % (table_annovar, self.avinput,self.species,human_db)
+		cmd += ','.join(self.genedb_list) +',' + ','.join(self.regiondb_list)+','+ ','.join(self.filterdb_list)
+		cmd += ' -operation ' + ','.join(['g']*len(self.genedb_list)) + ',' + ','.join(['r']*len(self.regiondb_list)) + ',' + ','.join(['f']*len(self.filterdb_list))
+		cmd += ' -nastring . --outfile %s -csvout' % (self.prefix + '.multianno')
+		print cmd
+		os.system(cmd)
+		outFile = self.prefix + '.multianno' + self.species + '_multianno.csv'
+		return outFile	'''
+	
+
+
+	def anno_multidb(self,db_list):
+		cmd = "%s %s --remove -buildver %s %s -protocol " % (table_annovar, self.avinput,self.species,self.db_path)
+		ope_list = []
+		for i in range(len(self.db)):
+			if db_list[i] in self.genedb_list:
+				ope_list.append('g')
+			elif db_list[i] in self.regiondb_list:
+				ope_list.append('r')
+			elif db_list[i] in self.filterdb_list:
+				ope_list.append('f')
+			else:
+				print "Input error or the query database does not exist"
+		cmd += ','.join(db_list)
+		cmd += ' -operation '+','.join(ope_list)
+		cmd += ' -nastring . --outfile %s' % (self.prefix + '.multianno')
+		print cmd
+		os.system(cmd)
+		outFile = self.prefix + '.multianno' + self.species + '_multianno.txt'
+		return outFile
+
 
 
