83f8685d07ce77ee68c15000a7d9e62861cc9be0
wangpenghui <wangpenghui@gene.ac>
2017-12-13 15:46:19 +0800

    add vcf2anno

--- bin/vcf2anno.py
+++ bin/vcf2anno.py
@@ -1,7 +1,24 @@
+#coding:utf-8
+import sys,os
+import argparse
+
 from vcf2anno.fmtvcf import fmtvcf
 from vc.....
 
 
+usage = """
+		python vcf2anno.py -i vcf -db dppath -out prefix
+	
+				--help			-h	usage;
+			[Required]
+				--input			-i	input vcf file;
+				--dbpath		-db	path of all the database;
+				--prefix		-p	prefix;
+			[Optional]
+				--annodb		-ad	wanted annonotaed database
+
+
+"""
 def vcf2anno(vcf,dbpath,prefix):
 
     fvcf = fmtvcf(vcf,prefix)
