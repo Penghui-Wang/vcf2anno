5bc0ff3b179e0b119d1d8012a8d1bfdeaedc3862
wangpenghui <wangpenghui@gene.ac>
2017-12-14 20:27:14 +0800

    change the input file

--- tests/test_fmtVCF.py
+++ tests/test_fmtVCF.py
@@ -3,7 +3,7 @@ sys.path.append("../")
 from vcf2anno.fmtVCF.fmtVCF import fmtVCF
 
 def test_fmtvcf():
-	vcf = "/lustre/users/yangrui/devwork/fastq2vcf/tests/mutect.freebayes.vcf"
+	vcf = "data/mutect.freebayes.vcf"
 	prex = "mutect.freebayes"
 	fmt = fmtVCF(vcf,prex)
 	assert fmt == "mutect.freebayes.left.vcf"
