c7dbd1d05165eca49abc3a15c0e31f303bbc3b73
wangpenghui <wangpenghui@gene.ac>
2017-12-20 10:01:30 +0800

    remove the extra_nav_links

--- docs/conf.py
+++ docs/conf.py
@@ -87,12 +87,12 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-	"extra_nav_links": {
-		" Github": "https://github.com/inducer/meshpy",
-		" Download Releases": "https://pypi.python.org/pypi/meshpy",
-		}
-	}
+#html_theme_options = {
+#	"extra_nav_links": {
+#		" Github": "https://github.com/inducer/meshpy",
+#		" Download Releases": "https://pypi.python.org/pypi/meshpy",
+#		}
+#	}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
