121f3d92dafcd398716cda767725f95d1352c881
wangpenghui <wangpenghui@gene.ac>
2017-12-20 09:38:59 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+form setuptools import setup
+
+setup(
+	name = 'vcf2anno',
+	version = '0.1',
+	author = 'wph',
+	author_email = 'wangpenghui@gene.ac',
+	url = 'https://github.com/Penghui-Wang/vcf2anno',
+	description = 'annovcf',
+	packages = ['vcf2anno'],
+	install_requires = [],
+	entry_points = {
+		''
+	}
+)
