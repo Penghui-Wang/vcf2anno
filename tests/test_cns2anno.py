09d70af66cf2cca89d626253591cb71c5a9bea35
wangpenghui <wangpenghui@gene.ac>
2018-02-05 16:35:10 +0800

    test ok

--- tests/test_cns2anno.py
+++ tests/test_cns2anno.py
@@ -1,8 +1,9 @@
 import os
-
+import sys
+sys.path.append('..')
 def test_cns2anno():
-	cns2anno = '/lustre/users/wangpenghui/DevWork/vcf2anno/bin/cns2anno.py'
-	cns = '/lustre/users/zhangxu/ProjWork/3.ctDNA/1.call_CNV/RD013-AgE02.call.cns'
+	cns2anno = '../bin/cns2anno.py'
+	cns = 'data/RD013-AgE02.call.cns'
 	prefix = '123'
 	cmd = "python %s -i %s -p %s" % (cns2anno,cns,prefix)
 	print cmd
