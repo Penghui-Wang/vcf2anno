6cf4071876d65f2afc35f60a0dc659b66917d09d
wangpenghui <wangpenghui@gene.ac>
2018-02-05 16:37:58 +0800

    test ok

--- tests/test_annoSV.py
+++ tests/test_annoSV.py
@@ -1,8 +1,10 @@
 import os
+import sys
+sys.path.append("../")
 
 def test_cns2anno():
-	annosv = '/lustre/users/wangpenghui/DevWork/vcf2anno/bin/annosv.py'
-	bdfl = '/lustre/users/zhangxu/ProjWork/3.ctDNA/2.call_SV/tog/tog.SV.output'
+	annosv = '../bin/annosv.py'
+	bdfl = 'data/tog.SV.output'
 	prefix = '234'
 	cmd = "python %s -i %s -p %s" % (annosv,bdfl,prefix)
 	print cmd
